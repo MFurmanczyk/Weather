c4f9d5ebd8d2901124cebbb533be10d7e216e323
MFurmanczyk <maciekf552@gmail.com>
2023-05-07 17:20:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/weather/ui/screens/WeatherScreen.kt
@@ -0,0 +1,19 @@
+package com.example.weather.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.weather.ui.theme.WeatherTheme
+
+@Composable
+fun WeatherScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WeatherScreenPreview() {
+    WeatherTheme {
+        WeatherScreen()
+    }
+}
